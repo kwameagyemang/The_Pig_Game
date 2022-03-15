ec2a24c133ae39f2c5e1add14847ef601ebc70fd
Kwame Agyemang <agyemangyeb@gmailcom>
2022-03-15 17:37:30 -0400

    commit

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 'use strict';
 // selctiing elements 
+const player0El = document.querySelector('.player--0')
+const player1El = document.querySelector('.player--1')
 const score0El = document.querySelector('#score--0');
 // getElementByID another way of selecting ids
 const score1El = document.getElementById('score--1');
@@ -16,7 +18,7 @@ score0El.textContent = 0;
 score1El.textContent = 0;
 diceEl.classList.add('hidden');
 
-const scores = [0,0];
+const scores = [0, 0];
 let currentScore = 0;
 let activePlayer = 0;
 // rollling dice functionality
@@ -41,9 +43,10 @@ btnRoll.addEventListener('click', function () {
     }else {
         // switch to next player
         // if activePlayer is 0 then switch Player to 1 else 0
+        document.getElementById(`current--${activePlayer}`).textContent = 0;
+        currentScore = 0;
         activePlayer = activePlayer === 0? 1 : 0;
-
-
+        player0El.classList.toggle()
 
     }
 
